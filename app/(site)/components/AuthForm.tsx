164dec9f433b71d9d200dedd3c56b98bc3671803
minhlip <minhlips2002@gmail.com>
2023-10-15 11:44:46 +0700

    Messme Auth UI and Initialize Prisma with MongoDB

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,147 @@
+'use client';
+
+import Button from '@/app/components/Button';
+import Input from '@/app/components/inputs/Input';
+import React, { useCallback, useState } from 'react';
+import { FieldValues, useForm, SubmitHandler } from 'react-hook-form';
+import AuthSocialButton from './AuthSocialButton';
+import { BsGithub, BsGoogle } from 'react-icons/bs';
+
+type Variant = 'LOGIN' | 'REGISTER';
+export default function AuthForm() {
+  const [variant, setVariant] = useState<Variant>('LOGIN');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === 'LOGIN') {
+      setVariant('REGISTER');
+    } else {
+      setVariant('LOGIN');
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    if (variant === 'REGISTER') {
+      //Axios Register
+    }
+
+    if (variant === 'LOGIN') {
+      //NextAuth Login
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    //NextAuth Social Login
+  };
+  return (
+    <div
+      className="
+        mt-8
+        sm:mx-auto
+        sm:w-full
+        sm:max-w-md
+   "
+    >
+      <div
+        className="
+        bg-white
+        border-[2px]
+        border-solid
+      border-gray-400
+        px-4
+        py-8
+        shadow
+        sm:rounded-lg
+        sm:px-10
+      "
+      >
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === 'REGISTER' && (
+            <Input
+              id="name"
+              label="Name"
+              type="text"
+              register={register}
+              error={errors}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            id="email"
+            label="Email"
+            register={register}
+            error={errors}
+            type="email"
+            disabled={isLoading}
+          />
+          <Input
+            id="password"
+            label="Password"
+            register={register}
+            error={errors}
+            type="password"
+            disabled={isLoading}
+          />
+          <div>
+            <Button disabled={isLoading} type="submit" fullWidth>
+              {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+            </Button>
+          </div>
+        </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="
+                absolute 
+                inset-0 
+                flex 
+                items-center
+              "
+            >
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              onClick={() => socialAction('github')}
+              icon={BsGithub}
+            />
+            <AuthSocialButton
+              onClick={() => socialAction('google')}
+              icon={BsGoogle}
+            />
+          </div>
+        </div>
+
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          {variant === 'LOGIN' ? 'New to Messme?' : 'Already have account?'}
+          <div onClick={toggleVariant} className="cursor-pointer underline">
+            {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
